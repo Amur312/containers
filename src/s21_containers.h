0abe1ff04c1d7d5197e6cbda14428837695ff168
Amur312 <dzarasovamur@gmail.com>
2023-07-22 19:42:49 +0300

    v0.1

---
+++ src/s21_containers.h
@@ -0,0 +1,10 @@
+// Файл s21_containers.h
+
+#pragma once
+
+#include "list/s21_list.h"
+// #include "s21_map.h"
+// #include "s21_queue.h"
+// #include "s21_set.h"
+// #include "s21_stack.h"
+// #include "s21_vector.h"
