b402698a073d0f423b722793109120ab61177caa
Amur312 <dzarasovamur@gmail.com>
2023-08-09 14:07:48 +0300

    21

--- src/tree/test.cpp
+++ src/tree/test.cpp
@@ -1,221 +1,62 @@
-#include "RedBlackTree.h"
+#include "s21_tree.h"
 #include <gtest/gtest.h>
 
-TEST(RedBlackTreeTest, InsertAndSize) {
-  s21::RedBlackTree<int> tree;
-  EXPECT_TRUE(tree.empty());
-  EXPECT_EQ(tree.size(), 0);
-  tree.insert(5);
-  EXPECT_FALSE(tree.empty());
-  EXPECT_EQ(tree.size(), 1);
+namespace s21 {
 
-  tree.insert(10);
-  EXPECT_EQ(tree.size(), 2);
-}
+class RedBlackTreeTest : public ::testing::Test {
+protected:
+  using Tree = RedBlackTree<int>; // Предполагается, что ваш класс дерева называется RedBlackTree и работает с int.
+  Tree tree;
 
-TEST(RedBlackTreeTest, Find) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-
-  auto it = tree.find(5);
-  EXPECT_NE(it, tree.cEnd());
-  EXPECT_EQ(*it, 5);
-
-  it = tree.find(15);
-  EXPECT_EQ(it, tree.cEnd());
-}
-
-TEST(RedBlackTreeTest, Erase) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-
-  auto it = tree.find(5);
-  tree.erase(it);
-  EXPECT_EQ(tree.size(), 1);
-
-  it = tree.find(5);
-  EXPECT_EQ(it, tree.cEnd());
-}
-
-TEST(RedBlackTreeTest, Iterator) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-
-  auto it = tree.cBegin();
-  EXPECT_EQ(*it, 5);
-  ++it;
-  EXPECT_EQ(*it, 10);
-  --it;
-  EXPECT_EQ(*it, 5);
-}
-
-TEST(RedBlackTreeTest, Merge) {
-  s21::RedBlackTree<int> tree1;
-  tree1.insert(5);
-  tree1.insert(10);
-
-  s21::RedBlackTree<int> tree2;
-  tree2.insert(15);
-  tree2.insert(20);
-
-  tree1.merge(tree2);
-
-  EXPECT_EQ(tree1.size(), 4);
-  EXPECT_EQ(tree2.size(), 0);
-  EXPECT_EQ(*tree1.find(5), 5);
-  EXPECT_EQ(*tree1.find(10), 10);
-  EXPECT_EQ(*tree1.find(15), 15);
-  EXPECT_EQ(*tree1.find(20), 20);
-}
-
-TEST(RedBlackTreeTest, UpperBound) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-  tree.insert(15);
-
-  auto it = tree.upperBound(7);
-  EXPECT_NE(it, tree.cEnd());
-  EXPECT_EQ(*it, 10);
-
-  it = tree.upperBound(15);
-  EXPECT_EQ(it, tree.cEnd());
-}
-
-TEST(RedBlackTreeTest, Clear) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-  tree.insert(15);
-
-  EXPECT_EQ(tree.size(), 3);
-  tree.clear();
-  EXPECT_TRUE(tree.empty());
-  EXPECT_EQ(tree.size(), 0);
-
-  auto it = tree.find(5);
-  EXPECT_EQ(it, tree.cEnd());
-}
-TEST(RedBlackTreeTest, InsertUnique_UniqueKey) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-
-  auto result = tree.insertUnique(10);
-  EXPECT_TRUE(result.second);
-  EXPECT_EQ(*result.first, 10);
-  EXPECT_EQ(tree.size(), 2);
-}
-
-TEST(RedBlackTreeTest, InsertUnique_NonUniqueKey) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-
-  auto result = tree.insertUnique(10);
-  EXPECT_FALSE(result.second);
-  EXPECT_EQ(*result.first, 10);
-  EXPECT_EQ(tree.size(), 2);
-}
-
-TEST(RedBlackTreeTest, OrderedIteration) {
-  s21::RedBlackTree<int> tree;
-  for (int i = 0; i < 10; ++i) {
-    tree.insert(i);
+  void SetUp() override {
+    // Инициализация дерева, если требуется.
   }
+};
 
-  int expectedValue = 0;
-  for (auto it = tree.cBegin(); it != tree.cEnd(); ++it) {
-    EXPECT_EQ(*it, expectedValue);
-    ++expectedValue;
-  }
+TEST_F(RedBlackTreeTest, InsertNode) {
+  tree.Insert(5);
+  tree.Insert(3);
+  tree.Insert(7);
+
+  // Проверяем корректное расположение элементов
+  EXPECT_EQ(tree.SearchMinimum(tree.Root())->key_, 3);
+  EXPECT_EQ(tree.SearchMaximum(tree.Root())->key_, 7);
 }
 
-TEST(RedBlackTreeTest, InsertDuplicateKeys) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(5);
-  tree.insert(5);
-  EXPECT_EQ(tree.size(), 3);
-}
-TEST(RedBlackTreeTest, InsertInReverseOrder) {
-  s21::RedBlackTree<int> tree;
-  for (int i = 9; i >= 0; --i) {
-    tree.insert(i);
-  }
+TEST_F(RedBlackTreeTest, ExtractNode) {
+  tree.Insert(5);
+  tree.Insert(3);
+  tree.Insert(7);
+  auto node = tree.ExtractNode(tree.Find(3)); // Предполагается, что метод Find возвращает итератор.
 
-  int expectedValue = 0;
-  for (auto it = tree.cBegin(); it != tree.cEnd(); ++it) {
-    EXPECT_EQ(*it, expectedValue);
-    ++expectedValue;
-  }
+  // Проверяем, что узел действительно извлечен
+  EXPECT_EQ(node->key_, 3);
+  EXPECT_EQ(tree.SearchMinimum(tree.Root())->key_, 5);
 }
 
-TEST(RedBlackTreeTest, EraseNonExistentKey) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
+TEST_F(RedBlackTreeTest, BlackHeightBalance) {
+  tree.Insert(5);
+  tree.Insert(3);
+  tree.Insert(7);
+  tree.Insert(1);
+  tree.Insert(9);
 
-  auto it = tree.find(15);
-  tree.erase(it);
-
-  EXPECT_EQ(tree.size(), 2);
-  EXPECT_EQ(*tree.find(5), 5);
-  EXPECT_EQ(*tree.find(10), 10);
+  // Проверяем корректный баланс черных узлов
+  EXPECT_GE(tree.ComputeBlackHeight(tree.Root()), 0);
 }
 
-TEST(RedBlackTreeTest, MergeWithEmptyTree) {
-  s21::RedBlackTree<int> tree1;
-  tree1.insert(5);
-  tree1.insert(10);
+TEST_F(RedBlackTreeTest, RedNodesCheck) {
+  tree.Insert(5);
+  tree.Insert(3);
+  tree.Insert(7);
 
-  s21::RedBlackTree<int> tree2;
-
-  tree1.merge(tree2);
-
-  EXPECT_EQ(tree1.size(), 2);
-  EXPECT_EQ(tree2.size(), 0);
+  // Проверяем, что красные узлы соответствуют правилам
+  EXPECT_TRUE(tree.checkRedNodes(tree.Root()));
 }
 
-TEST(RedBlackTreeTest, LowerBound) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-  tree.insert(15);
-
-  auto it = tree.lowerBound(7);
-  EXPECT_NE(it, tree.cEnd());
-  EXPECT_EQ(*it, 10);
-
-  it = tree.lowerBound(5);
-  EXPECT_EQ(*it, 5);
-}
-
-TEST(RedBlackTreeTest, EraseAndBalance) {
-  s21::RedBlackTree<int> tree;
-  tree.insert(5);
-  tree.insert(10);
-  tree.insert(15);
-  tree.insert(20);
-  tree.insert(25);
-  auto it = tree.find(15);
-  tree.erase(it);
-  EXPECT_EQ(tree.size(), 4);
-  EXPECT_EQ(*tree.find(5), 5);
-  EXPECT_EQ(*tree.find(10), 10);
-  EXPECT_EQ(*tree.find(20), 20);
-  EXPECT_EQ(*tree.find(25), 25);
-
-  it = tree.find(15);
-  EXPECT_EQ(it, tree.cEnd());
-}
-
-
+} // namespace s21
 
 int main(int argc, char **argv) {
-
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
